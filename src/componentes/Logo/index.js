c80409a9b6c01ae7b47e8fb83bc1a810fb1de6fd
Jamil Campeão <jamilcampeao@gmail.com>
2024-12-05 22:06:09 -0300

    Customizado tela com CSS

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -3,7 +3,12 @@ import logo from '../../imagens/logo.svg'
 function Logo() {
     return (
         <div className='logo'>
-            <img src={logo} alt='Logo da empresa'></img>
+            <img src={logo} 
+            alt='Logo da empresa' 
+            className='logo-img'
+            
+            >
+            </img>
             <p><strong>Alura Books</strong></p>
         </div>
     )
