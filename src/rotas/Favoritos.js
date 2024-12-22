15618dd8c7ef6ae177eeb28e440550b1c680dbdd
Jamil Campeão <jamilcampeao@gmail.com>
2024-12-22 13:22:38 -0300

    Criado serviços de Post e Delete de Livros Favoritos

--- src/rotas/Favoritos.js
+++ src/rotas/Favoritos.js
@@ -1,19 +1,73 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import Pesquisa from '../componentes/Pesquisa';
+import { getLivrosFavoritos } from '../servicos/favoritos';
 
 const AppContainer = styled.div`
     width: 100vw;
     height: 100vh;
+    background-image: linear-gradient(90deg,#002F52 35%,#326589 165%);
+`
+
+const ResultadoContainer = styled.div`
     display: flex;
-    flex-direction: column;
-    justify-content: space-between; /* Coloca espaço entre os componentes */
-    background-image: linear-gradient(90deg, #002F52 35%, #326589);
+    flex-wrap: wrap;
+    justify-content: center;
+`
+
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px 0;
+    cursor: pointer;
+    text-align: center;
+    padding: 0 100px;
+    p {
+        width: 200px;
+        color: #FFF;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
+
+const Titulo = styled.h2`
+    color: #FFF;
+    font-size: 36px;
+    text-align: center;
+    width: 100%;
+    padding-top: 35px
 `
 
 function Favoritos() {
+  const [favoritos, setFavoritos] = useState([]);
+
+  useEffect(() => {
+    fetchFavoritos()
+  }, []);
+
+    async function fetchFavoritos() {
+    const favoritosAPI = await getLivrosFavoritos();
+    setFavoritos(favoritosAPI);
+  }
   return (
     <AppContainer>
-      <Pesquisa/>
+      <div>
+        <Titulo>Aqui estão seus livros favoritos:</Titulo>
+        <ResultadoContainer>
+          {
+            favoritos.length !== 0 ? favoritos.map(favorito => (
+              <Resultado>
+                <p>{favorito.nome}</p>
+                {/* <img src={livroimg}/> */}
+              </Resultado>
+            )) : null
+          }
+        </ResultadoContainer>
+      </div>
       </AppContainer>
   );
 }
